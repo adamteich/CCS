fb360e992faf9bdd012a0cd84116cd14d0deae54
adamteich <59878782+adamteich@users.noreply.github.com>
2022-04-26 11:33:54 -0500

    maps NaN -> p(playing)=1

--- poker_simulation.m
+++ poker_simulation.m
@@ -5,25 +5,28 @@ reward(1) = 0;
 player_balance(1) = starting_cash;
 
 % by learning the probability that an opponent plays 1s when playing, we can understand their behavior and predict the utility of their moves
-P_competitor_plays_one_when_playing(1) = 1; % starts assuming no bluffing
+P_competitor_plays_one_when_playing(1) = 0.5; % starts assuming no bluffing
 t=1;
 bet_amount=50;
 while t<=num_hands && sum(reward)+starting_cash>0
     % 1. calculate expected utility of competitor's card
     U_competitor = P_competitor_plays_one_when_playing(t) * competitor_actions(t);
     if player_balance(t) >300
-        bet_amount= round(player_balance(t)* (rand()+1)/4) %if past a certain threshold, agent bets between 1/4 and 1/2 of current balance
+        bet_amount= round(player_balance(t)* (rand()+1)/3); %if past a certain threshold, agent bets between 1/3 and 2/3 of current balance
     end
    
     % 2. calculate expected utilities of our own actions (playing and folding)
     % U = [utility_of_playing, utility_of_folding]
     % to do: expected utilities should probably be more directly based on probabilities (rather than being discrete values: -10, 50, or -50)
     if competitor_actions(t) % if competitor plays...
-        if self_cards(t) + U_competitor + middle_cards(t) > 2 % ...and we expect to bust, then we expect utility of -50
+        if self_cards(t) + U_competitor + middle_cards(t) > 2.5 % ...and we expect to bust, then we expect utility of -50
             U(1) = -bet_amount;
         else % ...and we don't bust...
             if self_cards(t)>U_competitor % ...and we expect to win, then we expect utility of 50
                 U(1) = bet_amount;
+            if self_cards(t)==U_competitor
+                 U(1) = 5;
+            end
             else % ...and we expect to lose, then we expect utility of -50
                 U(1) = -bet_amount;
             end
@@ -34,7 +37,12 @@ while t<=num_hands && sum(reward)+starting_cash>0
     U(2) = -10; % folding always has utility of -10
     
     % 3. use expected utilities to determine our own best action
-    P_playing_card = exp(beta*U(1))/sum(exp(beta*U)); % probability of our agent playing rather than folding
+    output.U_1(t)=U(1);
+    P_playing_card = exp(beta*U(1))/sum(exp(beta*U))% probability of our agent playing rather than folding--check this
+    if isnan(P_playing_card)
+        P_playing_card=1;
+    end
+    output.P_playing(t)= P_playing_card;
     player_actions(t) = rand < P_playing_card; % probability --> action (1 == playing, 0 == folding)
     
     % 4. now that all players have made their choice, we can calculate the actual outcome and reward for our agent
@@ -48,7 +56,7 @@ while t<=num_hands && sum(reward)+starting_cash>0
                 if self_cards(t)>competitor_cards(t) % ...and win, then then reward=50
                     reward(t) = bet_amount;
                 elseif self_cards(t)==competitor_cards(t) % ...and tie, then then reward=0
-                    reward(t) = 0;
+                    reward(t) = 5;
                 else % ...and lose, then reward=-50
                     reward(t) = -bet_amount;
                 end
@@ -78,5 +86,6 @@ output.player_actions = player_actions;
 output.fold_rate = sum(output.reward(:) == -10) / length(reward);
 output.win_rate = sum(output.reward(:) >= 50) / length(reward);
 output.lose_rate = sum(output.reward(:) <= -50) / length(reward);
-output.tie_rate = sum(output.reward(:) == 0) / length(reward);
+output.tie_rate = sum(output.reward(:) == 5) / length(reward);
 output.win_from_opponent_fold = sum(output.reward(:) == 10) / length(reward);
+output.play_rate= output.win_rate+output.lose_rate+output.tie_rate
